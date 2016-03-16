5ded60cdbcd467d82a1343279c18ee584f1adf55
Paul den Arend <p.den.arend@gmail.com>
2016-03-16 13:22:03 +0000

    test3

--- config/environment.rb
+++ config/environment.rb
@@ -8,8 +8,8 @@ ActionMailer::Base.smtp_settings = {
   :address => 'smtp.sendgrid.net',
   :port => '587',
   :authentication => :plain,
-  :user_name => 'pdenarend',
-  :password => 'Marijke1!',
+  :user_name => ENV['SENDGGRID_USERNAME'],
+  :password => ENV['SENDGRID_PASSWORD'],
   :domain => 'heroku.com',
   :enable_starttls_auto => true
 }
